4234b1832ae1f40b377d389a5ea8fe90eb3a6504
Fotios Pechlivanis <fotispechli@gmail.com>
2021-10-18 16:32:46 +0200

    Add responsiveness

--- index.js
+++ index.js
@@ -11,14 +11,19 @@ theme.addEventListener("click", () => {
     }
 })
 
-let navListItems = document.querySelectorAll(".nav-list-item");
+let navListItems = document.querySelectorAll(".nav .nav-list-item");
+let navDropdownListItems = document.querySelectorAll(".nav-dropdown .nav-list-item");
 let sectionItems = document.querySelectorAll(".section");
 
 i = 0;
 
-sectionItems.forEach((element) => {
+sectionItems.forEach((section) => {
     navListItems[i].addEventListener('click', function () {
-        scrollToTargetAdjusted(element);
+        scrollToTargetAdjusted(section);
+    });
+
+    navDropdownListItems[i].addEventListener('click', function () {
+        scrollToTargetAdjusted(section);
     });
 
     i++;
@@ -40,6 +45,11 @@ root.addEventListener("scroll", () => {
     var scrolled = root.scrollTop;
     var offset = 120;
 
+    navUpdate(scrolled, offset);
+    navDropdownUpdate(scrolled, offset);
+});
+
+function navUpdate(scrolled, offset) {
     for (var i = 0; i < navListItems.length; i++) {
         if (scrolled > sectionItems[i].offsetTop - offset && (i == navListItems.length - 1  || scrolled < sectionItems[i + 1].offsetTop - offset)) {
             navListItems[i].classList.add('active');
@@ -47,7 +57,17 @@ root.addEventListener("scroll", () => {
             navListItems[i].classList.remove('active');
         }
     }
-});
+}
+
+function navDropdownUpdate(scrolled, offset) {
+    for (var i = 0; i < navDropdownListItems.length; i++) {
+        if (scrolled > sectionItems[i].offsetTop - offset && (i == navDropdownListItems.length - 1  || scrolled < sectionItems[i + 1].offsetTop - offset)) {
+            navDropdownListItems[i].classList.add('active');
+        } else {
+            navDropdownListItems[i].classList.remove('active');
+        }
+    }
+}
 
 let projects = document.querySelectorAll(".project-header");
 
@@ -55,4 +75,17 @@ projects.forEach((project) => {
     project.addEventListener("click", () => {
         project.parentNode.classList.toggle("open");
     })
+})
+
+let navHeader = document.querySelector(".nav-header");
+let backdrop = document.querySelector(".backdrop");
+
+navHeader.addEventListener("click", () => {
+    navHeader.parentNode.classList.toggle("open-nav");
+    backdrop.classList.toggle("show-backdrop");
+})
+
+backdrop.addEventListener("click", () => {
+    navHeader.parentNode.classList.remove("open-nav");
+    backdrop.classList.remove("show-backdrop");
 })
