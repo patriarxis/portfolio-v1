14b7eb5d5c02ad08a7bfb9e3fb84466d9a513367
Fotios Pechlivanis <fotispechli@gmail.com>
2022-08-23 12:40:25 +0300

    [Global] Add responsiveness for mobile.

---
+++ scripts/header.js
@@ -0,0 +1,11 @@
+var toggleNav = document.querySelector('.toggle-nav');
+var hamburger = document.querySelector('.hamburger');
+var x = document.querySelector('.x');
+var header = document.querySelector('.header');
+
+toggleNav.addEventListener('click', () => {
+  header.classList.toggle('open-nav');
+  header.classList.toggle('close-nav');
+  hamburger.classList.toggle('show-icon');
+  x.classList.toggle('show-icon');
+})
